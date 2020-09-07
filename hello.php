7c7ebf4fc0f9c7517e65f92609af0be51d171400
yossy0201 <yossy0201msk@gmail.com>
2020-09-07 23:03:02 +0900

    form

--- hello.php
+++ hello.php
@@ -1,5 +1,15 @@
-<?php
+<!DOCTYPE html>
+<meta charaset="utf-8">
+<head></head>
+<body>
 
-echo "hello world";
+<form method="GET" action="input.php">
+名前
+<input type="text" name="name"></input>
 
-?>
+<input type="submit" value="送信">
+
+
+</body>
+
+</html>
